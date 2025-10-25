1f9379bfc33127fcde511c74f90b3ae4d59d6c1f
Vivien Wallbaum <viviwbaum@gmail.com>
2025-10-25 20:32:28 +0200

    jacascript->typescript korrektur im Frontend-Test

--- frontend/src/__tests__/TodoApp.test.tsx
+++ frontend/src/__tests__/TodoApp.test.tsx
@@ -2,6 +2,9 @@ import { render, screen, fireEvent } from "@testing-library/react";
 import TodoApp from "../TodoApp.js";
 
 test("renders header text", () => {
-  render(<TodoApp token="fake-token" />);
+  render(
+    <TodoApp token="fake-token" setToken={jest.fn()} setLoggedOut={jest.fn()} />
+  );
+
   expect(screen.getByText(/FULLSTACK TODO APP/i)).toBeInTheDocument();
 });
